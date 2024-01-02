33a8e66356f1fe68089ba35d079267879f346c24
vlasik <vlasiigolovin@gmail.com>
2024-01-02 16:12:13 +0100

    upd

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 import os
-
+from ursina.prefabs.sky import Sky
+from ursina.shaders import basic_lighting_shader
+import random
+from perlin_noise import PerlinNoise
 app = Ursina()
 
+
+MAP_SIZE = 20
 PATH = os.getcwd()
 ASSETS_PATH = os.path.join(PATH, "assets")#game adress
 
@@ -35,19 +40,23 @@ class Block(Button):
         scale = 0.5,
         origin_y = 0.5,
         highlight_color=color.gray,
+        #shader=basic_lighting_shader
         )
     
     def input(self, key):
-        if  key== '1':
-            Block.id = 0
-        if  key== '2':
-            Block.id = 1
-        if  key== '3':
-            Block.id = 2
-        if key =='scrollup':
+        for i in range(10):
+            if  key== str(i):
+                Block.id = i
+
+        if key =='scroll up':
             Block.id -= 1
-        if key =='scrolldown':
-            Block.id += 1
+            if Block.id < 0:
+                Block.id = len(textures) - 1
+        if key =='scroll down':
+            if Block.id <= len(textures) - 1:
+                Block.id += 1
+            else:
+                Block.id = 0
         
         if self.hovered:
             if key == "left mouse down":
@@ -55,10 +64,20 @@ class Block(Button):
             if key == "right mouse down":
                 new_block = Block(position=self.position + mouse.normal, block_id=Block.id)
 
-for z in range(-10, 10):
-    for x in range(-10, 10):
-        new_block = Block((x, 0, z))
+noise = PerlinNoise(octaves=3, seed = random.randint(1,1000))
+for z in range(-MAP_SIZE, MAP_SIZE):
+    for x in range(-MAP_SIZE, MAP_SIZE):
+        height = noise([x*0.02, z* 0.02])
+        height = math.floor(height * 7.5)
+        new_block = Block((x, height, z))
+
+window.fullscreen = True
+scene.fog_density = 0.025
+scene.fog_color = color.rgb(120, 146, 232)
+
+pivot = Entity()
+DirectionalLight(parent=pivot, y=2, z=3, shadows=True)
 
 player = FirstPersonController()
-
+sky = Sky(texture = "sky_sunset")
 app.run()
